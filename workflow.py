c6533e5e4343a9f7babc05d00c55d4acb6e4b816
CamilaChahwan <cchahwan@itba.edu.ar>
2024-11-24 22:37:52 -0300

    Ejercicio 3.c

--- workflow.py
+++ workflow.py
@@ -34,6 +34,14 @@ if __name__ == "__main__":
         end_date="2025-01-01",    # Usar formato string adecuado
         condition_id="condition123"
     )
+     condition__id = send_resource_to_hapi_fhir(condition_resource, 'Condition')
 
     # Imprimir el recurso creado
     print(condition_resource)
+
+    # Ver el recurso de dispositivo creado
+    if device_id:
+        get_resource_from_hapi_fhir(condition__id, 'Condition')
+
+    print("\n")
+
